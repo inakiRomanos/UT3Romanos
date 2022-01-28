d68de06d86d736e7262c3d1a1a983e87a70f107d
inakiRomanos <80259560+inakiRomanos@users.noreply.github.com>
2022-01-28 09:27:27 +0100

    Tarea 2: Acceso a google maps

--- app/src/main/java/com/politecnico/dam/DashboardActivity.java
+++ app/src/main/java/com/politecnico/dam/DashboardActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -13,5 +15,14 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        final LinearLayout mapsButton = findViewById(R.id.layoutMaps);
+        mapsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_maps);
+            }
+        });
+
     }
 }
